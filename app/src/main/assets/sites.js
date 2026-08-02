2ffed76ca09248aa1f45c69614e22f9db8565949
b2444014-svg <b2444014@gmail.com>
2026-08-02 09:25:46 +0700

    Update sites.js

--- app/src/main/assets/sites.js
+++ app/src/main/assets/sites.js
@@ -5,10 +5,10 @@
 
 window.LOGO_URL = "https://res.cloudinary.com/xbdujq1q/image/upload/v1784994627/ICON_APK_NABUNG_BARU_lsfkl2.jpg";
 window.BG_URL   = "https://i.ibb.co/Txx3QDGR/background-apk-nabung.jpg";
-window.APP_NAME = "NABUNG77";
+window.APP_NAME = "KANTONGBET";
 
 /* Link Box Event & Pesan (live dari panel admin Worker) */
-window.INFO_URL = "https://playgame-notif.b2444014.workers.dev/info";
+window.INFO_URL = "https://kantongbet-notif.dewapokerqq25.workers.dev/info";
 
 
 /* =======================================================
@@ -22,17 +22,17 @@ window.INFO_URL = "https://playgame-notif.b2444014.workers.dev/info";
 
 window.SITES = [
   {
-    name: "NABUNG77",
+    name: "KANTONGBET",
     category: "SLOT",
     image: "https://res.cloudinary.com/xbdujq1q/image/upload/v1784994627/ICON_APK_NABUNG_BARU_lsfkl2.jpg",
     url: "https://cutt.ly/nabung77seobambu",
     premium: true
   },
   {
-    name: "GARASIPOKER",
+    name: "DEWAPOKERQQ",
     category: "PKV",
-    image: "https://i.ibb.co/gMdz90LD/logo-garasi-poker-di-dalam.jpg",
-    url: "https://cutt.ly/garasipoker-login"
+    image: "https://i.ibb.co/1fFQhvNn/LOGO-DEWAPOKER.png",
+    url: "https://cutt.ly/Dewapokerseobambu"
   }
   // Tambahkan game lain di sini, pisahkan dengan koma ( , )
 ];
